b401b12532921ed3fe9e2ee6fb956a27436c740f
Pavan Dharimireddy <pavandharimireddy.999@gmail.com>
2026-04-21 00:00:43 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    
+    <div>
+
+    </div>
       
   )
 }
