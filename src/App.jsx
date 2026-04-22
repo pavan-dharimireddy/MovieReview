5d506a940ec15285966f773fa8f974565a41a70f
Pavan Dharimireddy <pavandharimireddy.999@gmail.com>
2026-04-22 14:45:38 +0530

    "Router component and rounting links added"

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <div>
-      <MovieCard  movie={{title : "moviename"}}></MovieCard>
-      <Home></Home>
-    </div>
-      
+      <NavBar></NavBar>
+   <main className="main-content">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+   </main>
+      </div>
   )
 }
 
