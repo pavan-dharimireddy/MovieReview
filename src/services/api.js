343db1df63afb2343a01bc74fe9070c21b31aef2
Pavan Dharimireddy <pavandharimireddy.999@gmail.com>
2026-04-27 22:49:37 +0530

    api.js file added

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const fetchTrendingMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const fetchSearchResults = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&
+        query=${encodeURIComponent(query)
+    }`);
+    const data = await response.json();
+    return data.results;
+};
